e5f5a7a57417462286e754e0dcc38973d64d16fe
mhw32 <mike.wu@yale.edu>
2015-02-06 23:50:09 +0000

    starting the python code

--- python/formatData.py
+++ python/formatData.py
@@ -1,2 +1,12 @@
+# This file contains the functions used to load and edit data 
+# from MATLAB before any true labelling / classification is 
+# performed.
+
 from scipy.io import loadmat
-data = loadmat('../wu-example-feature-data.mat')
+
+# Read a .MAT file
+def mat2py(filepath, obj):
+	data = loadmat(filepath)[obj]
+	return data
+
+
